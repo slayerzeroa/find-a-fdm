5f02db125bf0a12839ec3d17327b6a5eb42036c4
Psyduck_20 <66348567+slayerzeroa@users.noreply.github.com>
2024-08-26 11:36:07 +0900

    fix: minutes data

--- 2024-2/ydm/data/hk.py
+++ 2024-2/ydm/data/hk.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 from helpful_functions import json2df
 
 #### 환경변수 세팅
-load_dotenv()
+load_dotenv(dotenv_path='C:/Users/slaye/VscodeProjects/find-a-fdm/2024-2/ydm/env/.env')
 APP_KEY = os.getenv("APP_KEY")
 APP_SECRET = os.getenv("APP_SECRET")
 
@@ -56,11 +56,55 @@ def get_access_token():
     access_token = response_data['access_token']
     return access_token
 
+try:
+    TOKEN = get_access_token()
+    txt_path = 'C:/Users/slaye/VscodeProjects/find-a-fdm/2024-2/ydm/env/token.txt'
+    with open(txt_path, 'w') as f:
+        f.write(TOKEN)
+except:
+    txt_path = 'C:/Users/slaye/VscodeProjects/find-a-fdm/2024-2/ydm/env/token.txt'
+    with open(txt_path, 'r') as f:
+        TOKEN = f.read()
 
-TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6ImViMWE1MjNiLThlOWUtNDUyMC04N2MwLTkwMDkzM2IzZDJjNiIsInByZHRfY2QiOiIiLCJpc3MiOiJ1bm9ndyIsImV4cCI6MTcyNDU1NzQ3NiwiaWF0IjoxNzI0NDcxMDc2LCJqdGkiOiJQU1VvTEg4MmJKbURCbVM3TmVwRkZURFYxMEh4bFYwUkJEd2EifQ.i-Q7Y7em1um9fKF4sWvK_LlIhYmZMJYigYPcqxKuDAyj5KzQ4eN1GbaKpInQUgv4Dmp7uB77b8QqmHdDGRRZRg"
+
+#### 당일 1분봉 데이터 요청
+headers = {
+    "content-type": "application/json; charset=utf-8",
+    "authorization": f"Bearer {TOKEN}",
+    "appkey": APP_KEY,
+    "appsecret": APP_SECRET,
+    "tr_id": "FHKST03010200",
+    "custtype": "P"
+}
+
+params = {
+    "fid_cond_mrkt_div_code": "J",
+    "fid_etc_cls_code": "",
+    "fid_input_hour_1": "090000",
+    "fid_input_iscd": "000660",
+    "fid_pw_data_incu_yn": "Y"
+}
+
+url = "https://openapivts.koreainvestment.com:29443/uapi/domestic-stock/v1/quotations/inquire-time-itemchartprice"
+
+res = requests.get(url, params=params, headers=headers)
+rescode = res.status_code
+if rescode == 200:
+    print(res.headers)
+    print(str(rescode) + " | " + res.text)
+else:
+    print("Error Code : " + str(rescode) + " | " + res.text)
+
+# print(res.json()['output'])
+print(res.json())
+
+pd.set_option('display.max_columns', None)
+print(json2df(res.json()['output2']))
 
 
-# #### 당일 1분봉 데이터 요청
+
+
+# #### 일별 주가 데이터
 # headers = {
 #     "content-type": "application/json; charset=utf-8",
 #     "authorization": f"Bearer {TOKEN}",
@@ -68,58 +112,29 @@ TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6ImViMW
 #     "appsecret": APP_SECRET,
 #     "tr_id": "FHKST01010400",
 #     "custtype": "P"
-#  }
+# }
 
 # params =  {
 #     "fid_cond_mrkt_div_code": "J",
-#     "fid_etc_cls_code": "",
-#     "fid_input_hour_1": "100000",
 #     "fid_input_iscd": "000660",
-#     "fid_pw_data_incu_yn": "Y"
-#  }
+#     "fid_org_adj_prc": "0000000001",
+#     "fid_period_div_code": "D"
+# }
 
-# url = "https://openapivts.koreainvestment.com:29443/oauth2/uapi/domestic-stock/v1/quotations/inquire-time-itemchartprice"
+# url = "https://openapivts.koreainvestment.com:29443/uapi/domestic-stock/v1/quotations/inquire-daily-price"
 
 # res = requests.get(url, params=params, headers=headers)
 # rescode = res.status_code
 # if rescode == 200:
-#     print(res.headers)
-#     print(str(rescode) + " | " + res.text)
+#     # print(res.headers)
+#     # print(str(rescode) + " | " + res.text)
+#     print('Success')
 # else:
 #     print("Error Code : " + str(rescode) + " | " + res.text)
 
 
-headers = {
-    "content-type": "application/json; charset=utf-8",
-    "authorization": f"Bearer {TOKEN}",
-    "appkey": APP_KEY,
-    "appsecret": APP_SECRET,
-    "tr_id": "FHKST01010400",
-    "custtype": "P"
-}
-print(headers)
+# # print(res.json()['output'])
+# print(res.json())
 
-params =  {
-    "fid_cond_mrkt_div_code": "J",
-    "fid_input_iscd": "000660",
-    "fid_org_adj_prc": "0000000001",
-    "fid_period_div_code": "D"
-}
-
-url = "https://openapivts.koreainvestment.com:29443/uapi/domestic-stock/v1/quotations/inquire-daily-price"
-
-res = requests.get(url, params=params, headers=headers)
-rescode = res.status_code
-if rescode == 200:
-    # print(res.headers)
-    # print(str(rescode) + " | " + res.text)
-    print('Success')
-else:
-    print("Error Code : " + str(rescode) + " | " + res.text)
-
-
-# print(res.json()['output'])
-print(res.json())
-
-pd.set_option('display.max_columns', None)
-print(json2df(res.json()['output']))
+# pd.set_option('display.max_columns', None)
+# print(json2df(res.json()['output']))
